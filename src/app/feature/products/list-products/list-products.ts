5afbe6ef14fee59486316af15ff3a8cffcb9c2f0
Pedro-0101 <pedro.tiete@outlook.com>
2025-11-28 21:40:26 -0300

    feat: design footer

--- src/app/feature/products/list-products/list-products.ts
+++ src/app/feature/products/list-products/list-products.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../../../core/header/header";
+import { Footer } from "../../../../core/footer/footer";
 
 @Component({
   selector: 'app-list-products',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './list-products.html',
   styleUrl: './list-products.css',
 })
