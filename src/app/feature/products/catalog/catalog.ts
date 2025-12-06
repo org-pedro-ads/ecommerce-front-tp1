dd02b3632c944ec417cf09c5799093c2fbad1225
Pedro-0101 <pedro.tiete@outlook.com>
2025-12-06 19:45:22 -0300

    feat: feito html e logica para buscar produtos na api

--- src/app/feature/products/catalog/catalog.ts
+++ src/app/feature/products/catalog/catalog.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Product } from '../../../models/product';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ProductService } from '../../../core/services/product/product.service';
+import { Header } from "../../../core/header/header";
+import { Footer } from "../../../core/footer/footer";
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './catalog.html',
   styleUrl: './catalog.css',
 })
 export class Catalog {
+
+  private productService = inject(ProductService);
+  loading = signal(true);
+
+  private products = toSignal<Product[], Product[]>(this.productService.getProducts().pipe(
+    finalize(() => this.loading.set(false))
+  ), {
+    initialValue: []
+  });  
+
+  listProducts = computed(() => {
+    const produtos = this.products();
+    return produtos.slice(0, 8);
+  });
 
 }
