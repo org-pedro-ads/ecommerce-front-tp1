c57e254ac7b7c9f156dfebb2e9ff21d651347c0a
Pedro-0101 <pedro.tiete@outlook.com>
2025-12-06 21:11:57 -0300

    feat: criado card-produto

--- src/app/feature/products/card-product/card-product.ts
+++ src/app/feature/products/card-product/card-product.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './card-product.css',
 })
 export class CardProduct {
+
+  @Input() produto: any = {
+    nome: 'Nome do Produto',
+    categoria: 'Geral',
+    descricao: 'Descrição do produto...',
+    preco: '0,00',
+    avaliacoes: 0
+  };
 
 }
